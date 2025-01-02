242169479dd1d665177469430bdb21c217f581f5
c-hano <wdq0304@naver.com>
2025-01-02 17:08:43 +0900

    sbb review

---
+++ src/main/java/com/example/ssb2/Ssb2Application.java
@@ -0,0 +1,13 @@
+package com.example.ssb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssb2/Ssb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
