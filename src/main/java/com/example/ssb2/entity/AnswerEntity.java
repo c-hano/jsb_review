64d4ea9522c2f1c887d3afb5c7ce23865010fd0a
c-hano <wdq0304@naver.com>
2025-01-03 14:50:52 +0900

    get AnswerEntity

---
+++ src/main/java/com/example/ssb2/entity/AnswerEntity.java
@@ -0,0 +1,27 @@
+package com.example.ssb2.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class AnswerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createTime;
+
+    @ManyToOne
+    private QuestionEntity question;
+
+}
