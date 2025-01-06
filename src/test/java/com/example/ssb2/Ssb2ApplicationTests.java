9acbf0895416cbac412b8cfd6bfe854c47c95976
c-hano <wdq0304@naver.com>
2025-01-06 11:48:18 +0900

    test findById

--- src/test/java/com/example/ssb2/Ssb2ApplicationTests.java
+++ src/test/java/com/example/ssb2/Ssb2ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.ssb2;
 
+import com.example.ssb2.entity.QuestionEntity;
+import com.example.ssb2.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Ssb2ApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+
+		Optional<QuestionEntity> oq = this.questionRepository.findById(1);
+		if (oq.isPresent()) {
+			QuestionEntity q = oq.get();
+			assertEquals("ssb가 무엇인가요?", q.getSubject());
+		}
 	}
 
 }
