ed91a171cfd5ed83c247054261a4aa32d8ce434f
c-hano <wdq0304@naver.com>
2025-01-06 14:53:48 +0900

    delete entitydata

--- src/test/java/com/example/ssb2/Ssb2ApplicationTests.java
+++ src/test/java/com/example/ssb2/Ssb2ApplicationTests.java
@@ -22,11 +22,12 @@ class Ssb2ApplicationTests {
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
+        assertEquals(2, this.questionRepository.count());
+        Optional<Question> q = this.questionRepository.findById(1);
+        assertTrue(q.isPresent());
+        Question q1 = q.get();
+        this.questionRepository.delete(q1);
+        assertEquals(1, this.questionRepository.count());
     }
 }
 
