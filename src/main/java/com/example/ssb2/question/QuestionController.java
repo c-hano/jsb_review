e2d1804d3d79c6a3991b26cb7190ad140b0e9cb8
c-hano <wdq0304@naver.com>
2025-01-07 10:51:21 +0900

    set mapping with resposeBody

---
+++ src/main/java/com/example/ssb2/question/QuestionController.java
@@ -0,0 +1,15 @@
+package com.example.ssb2.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuestionController {
+
+    @GetMapping("list")
+    @ResponseBody
+    public String list() {
+        return "question list";
+    }
+}
